f6f2617a0455a98d6f68c1dccd2c609f54c7113f
Erkan Emal <hackkappaxd@gmail.com>
2025-01-08 15:20:28 +0100

    Deleted old stuff

--- client/config.lua
+++
@@ -1,7 +0,0 @@
-Config = {}
-
--- Default Keybind for Admin Menu (can be changed in-game with ox_lib key mappings)
-Config.OpenMenuKey = 'F5'
-
--- Targeting system to use ('ox-target' or 'qb-target')
-Config.TargetSystem = 'qb-target'
--- sql.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `persistent_npcs` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `model` VARCHAR(255) NOT NULL,
-    `x` FLOAT NOT NULL,
-    `y` FLOAT NOT NULL,
-    `z` FLOAT NOT NULL,
-    `emote` VARCHAR(255) DEFAULT NULL
-);
