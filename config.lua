bfeef42a416f9d70538785f7742e7f14166a84da
Erkan Emal <hackkappaxd@gmail.com>
2025-01-08 16:11:35 +0100

    edited config for astro rp

--- config.lua
+++ config.lua
@@ -2,16 +2,16 @@ Config = {}
 
 Config.NPCs = {
     {
-        coords = vector3(-1037.7, -2737.9, 20.169), -- Plane shop NPC
-        heading = 340.0,
+        coords = vector3(-993.7590, -2948.5032, 13.9574), -- Plane shop NPC
+        heading = 55.8697,
         model = 'a_m_y_business_02',
         animation = { dict = 'amb@world_human_hang_out_street@male_a@base', name = 'base' }
     },
     {
-        coords = vector3(441.2, -981.3, 30.6), -- Surgery NPC
-        heading = 90.0,
+        coords = vector3(289.9805, -1442.9890, 29.9666), -- Surgery NPC
+        heading = 329.1189,
         model = 's_m_m_doctor_01',
-        animation = { dict = 'amb@code_human_cross_road@male@idle_a', name = 'idle_b' }
+        animation = { dict = 'amb@code_human_wander_clipboard@male@base', name = 'clipboard' }
     }
     -- Add more here
 }
