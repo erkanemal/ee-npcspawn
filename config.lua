e10a98e19ef8324a5429d2ed029f557e8828f2c1
Erkan Emal <hackkappaxd@gmail.com>
2025-01-08 15:37:35 +0100

    added anim list and readme

--- config.lua
+++ config.lua
@@ -2,15 +2,16 @@ Config = {}
 
 Config.NPCs = {
     {
-        coords = vector3(-1037.7, -2737.9, 20.169), -- Plan shop NPC
+        coords = vector3(-1037.7, -2737.9, 20.169), -- Plane shop NPC
         heading = 340.0,
-        model = 'a_m_m_farmer_01',
+        model = 'a_m_y_business_02',
         animation = { dict = 'amb@world_human_hang_out_street@male_a@base', name = 'base' }
     },
     {
         coords = vector3(441.2, -981.3, 30.6), -- Surgery NPC
         heading = 90.0,
-        model = 's_m_y_cop_01',
+        model = 's_m_m_doctor_01',
         animation = { dict = 'amb@code_human_cross_road@male@idle_a', name = 'idle_b' }
     }
+    -- Add more here
 }
